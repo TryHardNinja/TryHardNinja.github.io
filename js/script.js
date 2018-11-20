a2052a908107271f6c1a4d8afd9c94e4accf4e5f
tryhardninja <tryhardsariel@gmail.com>
2018-11-20 21:41:56 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -20096,15 +20096,9 @@
 	//
 	//
 	//
-	var ModalForm = {
-	  props: ['email', 'password'],
-	  template: "\n        <form action=\"\">\n            <div class=\"modal-card\" style=\"width: auto\">\n                <header class=\"modal-card-head\">\n                    <p class=\"modal-card-title\">Login</p>\n                </header>\n                <section class=\"modal-card-body\">\n                    <b-field label=\"Email\">\n                        <b-input\n                            type=\"email\"\n                            :value=\"email\"\n                            placeholder=\"Your email\"\n                            required>\n                        </b-input>\n                    </b-field>\n\n                    <b-field label=\"Password\">\n                        <b-input\n                            type=\"password\"\n                            :value=\"password\"\n                            password-reveal\n                            placeholder=\"Your password\"\n                            required>\n                        </b-input>\n                    </b-field>\n\n                    <b-checkbox>Remember me</b-checkbox>\n                </section>\n                <footer class=\"modal-card-foot\">\n                    <button class=\"button\" type=\"button\" @click=\"$parent.close()\">Close</button>\n                    <button class=\"button is-primary\">Login</button>\n                </footer>\n            </div>\n        </form>\n    "
-	};
 	var script = {
 	  name: 'WidgetBody',
-	  components: {
-	    ModalForm: ModalForm
-	  },
+	  components: {},
 	  data: function data() {
 	    return {
 	      isComponentModalActive: false,
